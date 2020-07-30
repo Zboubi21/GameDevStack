a780425df60ad934bb09329346a21b41e8321af8
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2020-07-30 19:42:58 +0200

    [Feature] Added VersionVisualizer.cs

---
+++ Sources/VersionControl/VersionVisualizer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace GameDevStack.VersionControl
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class VersionVisualizer : MonoBehaviour
+    {
+        private TextMeshProUGUI m_Text = null;
+
+        private void Awake()
+        {
+            m_Text = GetComponent<TextMeshProUGUI>();
+            m_Text.text = string.Concat(Application.version);
+        }
+    }
+}
