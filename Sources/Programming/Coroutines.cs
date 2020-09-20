07342a4b433fdbe7b97f893fd3559e8050652987
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2020-09-20 16:37:20 +0200

    [Coroutine Helper] Invoke methods with delay

---
+++ Sources/Programming/Coroutines.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using GameDevStack.Patterns;
+
+namespace GameDevStack.Programming
+{
+    public class Coroutines : SingletonMonoBehaviour<Coroutines>
+    {
+        public static Coroutine InvokeWithDelay(Action action, float delay)
+        {
+            return Instance.StartCoroutine(CoroutineDelay(action, delay));
+        }
+
+        public static void StopInvoke(Coroutine coroutine)
+        {
+            if (coroutine != null)
+                Instance.StopCoroutine(coroutine);
+        }
+
+        private static IEnumerator CoroutineDelay(Action action, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            action?.Invoke();
+        }
+    }
+}
