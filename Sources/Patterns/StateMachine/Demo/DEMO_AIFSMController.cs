18ff693c71ccd72b4bee0a84026c28dc3842f00d
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2021-04-10 14:58:50 +0200

    [StateMachine] Deleted FSM inspiration and updated lines of code

--- Sources/Patterns/StateMachine/Demo/DEMO_AIFSMController.cs
+++ Sources/Patterns/StateMachine/Demo/DEMO_AIFSMController.cs
@@ -10,23 +10,10 @@ namespace GameDevStack.Demos
         {
             defaultState = State.Move;
 
-            // V1 : Court mais possible d'avoir des erreurs
-            //states[1] = new DEMO_AIMoveState(this);
-
-            // V2 : Long mais impossible d'avoir des erreurs
-            //states = FSM<State>.ReplaceState(states, new DEMO_MoveState(this), new DEMO_AIMoveState(this));
-
-            // V3 : Presque parfait (trouver un autre endroit un ranger la fonction : "ReplaceState")
-            //states = FSM<State>.ReplaceState(states, State.Move, new DEMO_AIMoveState(this));
-
-            // V4 : Parfait ? (le faire en extension plutôt ?)
-            //states = FSMUtilities.ReplaceType(states, State.Move, new DEMO_AIMoveState(this));
-
-            // V5 : Avec une extension
             //states.ReplaceState(State.Idle, new DEMO_AIIdleState(this));
             states.ReplaceState(State.Move, new DEMO_AIMoveState(this));
 
-            // V6 : Version avancé pour override plusieurs states (un peu lourd, plutôt privilégier la V5)
+            // Version avancé pour override plusieurs states
             //states.ReplaceStates(new List<State>
             //{
             //    State.Idle,
