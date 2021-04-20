e1f569b33dab2832dda863a6f52e77be01f89a32
JeremyPayreLaptop <jeremy.payre@dualcat.io>
2021-04-20 23:45:13 +0200

    [Extensions][Vector] Added vector extensions to only set x, y, or z values

---
+++ Sources/Programming/Extensions/VectorExtensions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+    /**********
+    * Vector2 *
+    **********/
+    public static Vector2 SetX(this Vector2 v, float x)
+    {
+        v.x = x;
+        return v;
+    }
+
+    public static Vector2 SetY(this Vector2 v, float y)
+    {
+        v.y = y;
+        return v;
+    }
+
+    /**********
+    * Vector3 *
+    **********/
+    public static Vector3 SetX(this Vector3 v, float x)
+    {
+        v.x = x;
+        return v;
+    }
+
+    public static Vector3 SetY(this Vector3 v, float y)
+    {
+        v.y = y;
+        return v;
+    }
+
+    public static Vector3 SetZ(this Vector3 v, float z)
+    {
+        v.z = z;
+        return v;
+    }
+}
